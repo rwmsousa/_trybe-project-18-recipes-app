26d058b3e25d09c8182fc770bf01698ecf871db1
Caê Calçolari <eduardokae01@hotmail.com>
2021-09-21 18:18:35 -0300

    botao habilita depois de email e senha validos

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
 import App from './App';
+import { screen } from '@testing-library/dom';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testa pagina Login.js', () => {
+  test('verifica que existe um título h1', () => {
+    renderWithRouter(<App />);
+
+    const titleH1 = screen.getByRole('heading', {
+      name: /login/i,
+      level: 1,
+    });
+
+    expect(titleH1).toBeInTheDocument();
+  })
+
+  test('Verifica que existem 2 imputs, email e senha', () => {
+    renderWithRouter(<App />);
+
+    const getEmailInput = screen.getByTestId('email-input');
+    const getPasswordInput = screen.getByTestId('password-input');
+
+    expect(getEmailInput).toBeInTheDocument();
+    expect(getPasswordInput).toBeInTheDocument();
+  })
+
+  test('Verifica que existe um botão com texto "Entrar" e começa desativado', () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByRole('button', {
+      name: 'Entrar',
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisable();
+  });
+})
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [button, setButton] = useState('true');
+
+  const handleChange = ({ target: { value } }) => {
+    const validEmail = /\S+@\S+\.\S+/;
+    const magicNumber = 5;
+    setPassword(value);
+
+    if (validEmail.test(email) && password.length >= magicNumber) {
+      setButton(!button);
+    }
+  };
+
   return (
     <section>
       <h1> Login </h1>
@@ -11,6 +25,7 @@ function Login() {
           id="input-email"
           placeholder="Insira seu e-mail"
           data-testid="email-input"
+          onChange={ (e) => setEmail(e.target.value) }
         />
       </label>
       <label htmlFor="input-password">
@@ -20,11 +35,13 @@ function Login() {
           id="input-password"
           placeholder="Insira sua senha"
           data-testid="password-input"
+          onChange={ handleChange }
         />
       </label>
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ button }
       >
         Entrar
       </button>
