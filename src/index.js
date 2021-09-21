972cd82d34bf4d0709f94af4d8a37b7f8e72b947
Sethano <vitorcard1720@gmail.com>
2021-09-21 16:05:08 -0300

    arrumando erro no app.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Provider from './Context/Provider';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider>
       <App />
     </Provider>
-  </BrowserRouter>
-, document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
