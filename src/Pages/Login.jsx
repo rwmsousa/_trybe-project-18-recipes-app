9f30827e522156b9cbee1f3b7786d303c679a6f5
Caê Calçolari <eduardokae01@hotmail.com>
2021-09-21 16:59:29 -0300

    faz esqueleto basico form e helper renderWithRouter para tests

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1> Login </h1>
+    <section>
+      <h1> Login </h1>
+      <label htmlFor="input-email">
+        Email:
+        <input
+          type="email"
+          id="input-email"
+          placeholder="Insira seu e-mail"
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="input-password">
+        Senha:
+        <input
+          type="password"
+          id="input-password"
+          placeholder="Insira sua senha"
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </section>
   );
 }
 
