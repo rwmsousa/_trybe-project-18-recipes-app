9728ec8ab8fa33b6b09d2896630b283b021123a6
Sethano <vitorcard1720@gmail.com>
2021-09-22 14:53:20 -0300

    resolvento erro de stylelint

---
+++ src/Pages/Explorer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+
+function Explorer() {
+  return (
+    <div>
+      <h1> Explorer </h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default Explorer;
