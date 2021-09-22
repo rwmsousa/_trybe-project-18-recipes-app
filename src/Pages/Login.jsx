70c35108f68bc565d33d025b723a1866ee118362
Caê Calçolari <eduardokae01@hotmail.com>
2021-09-22 00:23:21 -0300

    refatora Header

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Form from '../Components/Form';
+import Form from '../components/Form';
 import Context from '../Context/Context';
 import './login.css';
 
