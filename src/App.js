6a00eea36532b9e3dc6c654d0cac007a594aec8f
ricardo-sousa <rwmsousa@gmail.com>
2021-09-23 15:55:58 -0300

    criando rotas pages

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
-import { Login, Foods, Drinks, Profile, Explorer, DrinkDetail, FoodDetail } from './Pages/Index';
+import {
+  Login,
+  Foods,
+  Drinks,
+  Profile,
+  Explorer,
+  DrinkDetail,
+  FoodDetail,
+  FoodInProgress,
+  DrinkInProgress,
+  RecipesMade,
+  FavoriteRecipes,
+  ExplorerFoods,
+  ExploreByAreaFood,
+  ExploreByIngredientFood,
+  ExploreByIngredientDrinks,
+  ExplorerDrinks
+} from './Pages/Index';
 
 function App() {
   return (
     <Switch>
       <Route path="/comidas" component={ Foods } />
+      <Route path="/comidas/:id" component={ FoodDetail } />
+      <Route path="/comidas/:id/in-progress" component={ FoodInProgress } />
       <Route path="/bebidas" component={ Drinks } />
-      <Route path="/profile" component={ Profile } />
+      <Route path="/bebidas/:id" component={ DrinkDetail } />
+      <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
       <Route path="/explorar" component={ Explorer } />
-      <Route path="/bebidas/{id}" component={ DrinkDetail } />
-      <Route path="/comidas/{id}" component={ FoodDetail } />
+      <Route path="/explorar/comidas" component={ ExplorerFoods } />
+      <Route path="/explorar/comidas/area" component={ ExploreByAreaFood } />
+      <Route path="/explorar/comidas/ingredientes" component={ ExploreByIngredientFood } />
+      <Route path="/explorar/bebidas" component={ ExplorerDrinks } />
+      <Route path="/explorar/bebidas/ingredientes" component={ ExploreByIngredientDrinks } />
+      <Route path="/perfil" component={ Profile } />
+      <Route path="/receitas-feitas" component={ RecipesMade } />
+      <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
