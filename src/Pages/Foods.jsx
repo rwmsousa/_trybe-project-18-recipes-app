0a232f55e5df3f043736dd7160bd480e81af508a
Caê Calçolari <eduardokae01@hotmail.com>
2021-09-23 15:09:34 -0300

    commit para fazer o pull

--- src/Pages/Foods.jsx
+++ src/Pages/Foods.jsx
@@ -32,6 +32,23 @@ function Foods() {
     fetchCategories();
   }, []);
 
+  const HandleClick = ({ target: { name } }) => {
+    const results = fetch(`www.themealdb.com/api/json/v1/1/filter.php?c=${name}`)
+      .then((response) => (
+        response
+          .json()
+          .then((data) => data)
+      ));
+    console.log(results);
+  };
+  // async function HandleClick({ target: { name } }) {
+  //   const results = await fetch(
+  //     `www.themealdb.com/api/json/v1/1/filter.php?c=${name}`,
+  //   );
+  //   const data = await results.json();
+  //   console.log(data);
+  // };
+
   return (
     <div className="foods">
       <Header />
@@ -41,6 +58,8 @@ function Foods() {
             type="button"
             key={ category.strCategory }
             data-testid={ `${category.strCategory}-category-filter` }
+            name={ category.strCategory }
+            onClick={ (event) => HandleClick(event) }
           >
             { category.strCategory }
           </button>
---
+++ src/tests/food.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testa pagina Food.js', () => {
+  test('verifica que existem dois icones no header', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // history.push('/comidas');
+
+    // const img = screen.getByTestId('profile-top-btn');
+    // console.log(img);
+    // expect(img).toBeInThDocument();
+  });
+});
