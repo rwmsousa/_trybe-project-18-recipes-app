3027080207859bf7063f4f95a213916d44733d2b
ricardo-sousa <rwmsousa@gmail.com>
2021-09-24 19:36:12 -0300

    encerrando o dia

--- src/Pages/RecipesMade.jsx
+++ src/Pages/RecipesMade.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext } from 'react';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Context from '../Context/Context';
 
@@ -16,7 +15,49 @@ function RecipesMade() {
   return (
     <div>
       <Header />
-      <Footer />
+      <div className="buttonsRecipesMade">
+        <button type="button" data-testid="filter-by-all-btn">
+          All
+        </button>
+        <button type="button" data-testid="filter-by-food-btn">
+          Food
+        </button>
+        <button type="button" data-testid="filter-by-drink-btn">
+          Drinks
+        </button>
+      </div>
+      {/* <div className="cardRecipes">
+        <div className="divImageCard">
+          <img
+            src=""
+            alt=""
+            className="immageCard"
+            data-testid={ `"${index}-horizontal-image"` }
+          />
+        </div>
+        <div className="infoCard">
+          <p
+            className="categoryCard"
+            data-testid={ `"${index}-horizontal-top-text"` }
+          >
+            { }
+          </p>
+          <p className="titleCard" data-testid={ `"${index}-horizontal-name"` }>{}</p>
+          <p className="dateCard" data-testid={ `"${index}-horizontal-done-date"` }>{}</p>
+          <img
+            src=""
+            alt=""
+            className="shareCard"
+            data-testid={ `"${index}-horizontal-share-btn"` }
+          />
+          <p
+            className="tagCard"
+            data-testid={ `${index}-${tagName}-horizontal-tag` }
+          >
+            { }
+          </p>
+        </div>
+      </div> */}
     </div>
   );
 }
