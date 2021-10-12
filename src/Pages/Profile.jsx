9178f083bcc61f214d2b98aa64cc04a8488b69a5
ricardo-sousa <rwmsousa@gmail.com>
2021-10-12 17:56:49 -0300

    css profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Context from '../Context/Context';
+import '../css/profile.css';
 
 function Profile() {
   const { setCurrentPage, setSearchButton } = useContext(Context);
@@ -28,21 +29,37 @@ function Profile() {
   return (
     <div>
       <Header />
-      { userEmail ? <p data-testid="profile-email">{ userEmail.email }</p> : <p>User</p>}
-      <button type="button" data-testid="profile-done-btn">
-        <a href="receitas-feitas">Receitas Feitas</a>
-      </button>
-      <button type="button" data-testid="profile-favorite-btn">
-        <a href="/receitas-favoritas">Receitas Favoritas</a>
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => clickExit() }
-      >
-        Sair
-      </button>
-
+      <div className="content-profile">
+        {userEmail ? (
+          <p data-testid="profile-email" className="email">
+            {userEmail.email}
+          </p>
+        ) : (
+          <p>User</p>
+        )}
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          className="profile-done-btn"
+        >
+          <a href="receitas-feitas">Receitas Feitas</a>
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          className="profile-favorite-btn"
+        >
+          <a href="/receitas-favoritas">Receitas Favoritas</a>
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => clickExit() }
+          className="exit"
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
